083aea1f9cde43224979e90e43bde8daab3ba123
Elena Mamontova <helenh463@gmail.com>
2018-11-08 16:24:54 +0200

    Checking argv[1] added in the main() function.

--- server.cpp
+++ server.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include <cstring>
+
 int main(int argc, char * argv[])
 {
-    return 1;
+    float version = 1.0;
+
+    if (argc > 1) {
+        if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--version")){
+            std::cout << "Version " << version << "\n";
+        } else {
+            std::cout << "Argument is not version\n";
+            return -1;
+        }
+    } else {
+        std::cout << "Not arguments\n";
+        return -1;
+    }
+
+    return 0;
 }
