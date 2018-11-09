be1c2894f2c1038511a3d4709fb975e9dc76189a
Elena Mamontova <helenh463@gmail.com>
2018-11-09 15:07:21 +0200

    Command line parameter '--help' check added.

--- server.cpp
+++ server.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 
 int main(int argc, char * argv[])
 {
-    float version = 1.0;
+    std::string version = "1.0";
 
     if (argc > 1) {
         if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--version")){
             std::cout << "Version " << version << "\n";
         } else {
-            std::cout << "Argument is not version\n";
-            return -1;
+            if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")){
+                std::cout << "Usage:\n";
+                std::cout << "server [-v/--version] [-h/--help]\n\n";
+
+                std::cout << "--version, -v - server version;\n";
+                std::cout << "--help, -h - show this text.\n";
+            } else {
+                std::cout << "Unknown command line argument\n";
+                return -1;
+            }
         }
     } else {
-        std::cout << "Not arguments\n";
+        std::cout << "No arguments\n";
         return -1;
     }
 
