8a1db8d070a3245bf313b1dc5ca722429273ae38
Elena Mamontova <helenh463@gmail.com>
2018-12-03 17:26:20 +0200

    Added client.cpp file. Added parsing command line parameters.

---
+++ client.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+void reference();
+
+int main(int argc, char* argv[])
+{
+    std::string version = "1.0";
+    std::string server_address;
+    std::string file_name;
+    std::string command;
+
+    if (argc < 2)
+    {
+        reference();
+        return 1;
+    }
+
+    for (int i = 1; i < argc; ++i)
+    {
+        if ((!strcmp(argv[i], "-a") || !strcmp(argv[i], "--address")))
+        {
+            if (i + 1 == argc)
+            {
+                std::cerr << argv[i] << " needs and argument - a server address.\n";
+                return 1;
+            }
+            server_address = argv[++i];
+        }
+        else if ((!strcmp(argv[i], "-g") || !strcmp(argv[i], "--get")))
+        {
+            if (i + 1 == argc)
+            {
+                std::cerr << argv[i] << " needs and argument - a file name get.\n";
+                return 1;
+            }
+            file_name = argv[++i];
+            command = "get";
+        }
+        else if ((!strcmp(argv[i], "-p") || !strcmp(argv[i], "--put")))
+        {
+            if (i + 1 == argc)
+            {
+                std::cerr << argv[i] << " needs and argument - a file name put.\n";
+                return 1;
+            }
+            file_name = argv[++i];
+            command = "put";
+        }
+       else if ((!strcmp(argv[i], "-l") || !strcmp(argv[i], "--list")))
+        {
+            command = "list";
+        }
+        else if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--version"))
+        {
+            std::cout << "Version " << version << "\n";
+            return 0;
+        }
+        else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
+        {
+            reference();
+            return 0;
+        }
+        else
+        {
+            std::cerr << "Unknown command line argument\n";
+            return 1;
+        }
+    }
+}
+
+void reference()
+{
+    std::cout << "Usage:\n";
+    std::cout << "client -a/--address <ip_address:port> -g/--get <name> -p/--put <name> -l/--list [-v/--version] [-h/--help]\n\n";
+
+    std::cout << "--address, -a <ip_address:port> - specifies local IP address and port of server;\n";
+    std::cout << "--get, -g <name> - specified the file name to get from server;\n";
+    std::cout << "--put, -p <name> - specified the file name to write to the server;\n";
+    std::cout << "--list, -l - gets file list from server;\n";
+    std::cout << "--version, -v - server version;\n";
+    std::cout << "--help, -h - show this text.\n";
+}
