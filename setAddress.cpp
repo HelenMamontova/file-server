8857fa437d69b7cf786df5cac0f68babe0a8d9a7
Elena Mamontova <helenh463@gmail.com>
2018-12-12 18:09:42 +0200

    The directive #include <netinet/in.h> removed. The construction 'if-else' changed to construction 'if'.

--- setAddress.cpp
+++ setAddress.cpp
@@ -2,7 +2,6 @@
 #include <cstring>
 #include <string>
 #include <stdlib.h> //exit
-#include <netinet/in.h> //struct sockaddr_in
 #include <arpa/inet.h> //inet_aton
 
 #include "setaddress.h"
@@ -18,11 +17,8 @@ bool setAddress(const std::string& address, struct sockaddr_in* local)
         std::cerr << address << " - incorrect address\n";
         return false;
     }
-    else
-    {
-        ip_address = address.substr(0, pos);
-        port = address.substr(pos + 1, address.length() - pos - 1);
-    }
+    ip_address = address.substr(0, pos);
+    port = address.substr(pos + 1, address.length() - pos - 1);
 
     bzero(local, sizeof (*local));
     local->sin_family = AF_INET;
@@ -40,9 +36,7 @@ bool setAddress(const std::string& address, struct sockaddr_in* local)
         std::cerr << port << " - unknown port\n";
         return false;
     }
-    else
-    {
-        local->sin_port = htons(port_num);
-    }
+    local->sin_port = htons(port_num);
+
     return true;
 }
