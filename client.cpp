895989fb646e2f48bc5354239c366e1c6fd0795a
Elena Mamontova <helenh463@gmail.com>
2019-03-11 16:35:36 +0200

    The construction if-else-if server responses analysis improved in the sendFile() function.

--- client.cpp
+++ client.cpp
@@ -176,90 +176,84 @@ int sendFile(int s, const std::string& file_name)
         std::cerr << error_message << "\n";
         return 1;
     }
-    else if (file_name_allow == 129)
-    {
-
-    // определение длины файла
-        struct stat st_buff;
-        res = stat(file_name.c_str(), &st_buff);
-        if (res < 0)
-        {
-            std::cerr << "Stat call error. " << strerror(errno) << "\n";
-            return 1;
-        }
-
-        uint32_t filesize = st_buff.st_size;
-
-    //отправка серверу длины файла
-        res = send(s, &filesize, sizeof(filesize), 0);
-        if (res < 0 || res != sizeof(filesize))
-        {
-            std::cerr << "Send call error file size. " << strerror(errno) << "\n";
-            return 1;
-        }
-
-    //открытие клиентом файла
-        std::ifstream fin(file_name);
-        if (!fin)
-        {
-            std::cerr << file_name << "File not open.\n";
-            return 1;
-        }
-
-    // чтение файла в буфер
-        char buff[1024] = {0};
-        while (!fin.eof())
-        {
-            fin.read(buff, 1024);
-
-    // отправка содержимого буфера серверу
-            if (fin.gcount() > 0)
-            {
-                res = send(s, buff, fin.gcount(), 0);
-                if (res < 0 || res != (int)fin.gcount())
-                {
-                    std::cerr << "Send call error buff. " << strerror(errno) << "\n";
-                    return 1;
-                }
-            }
-
-        }
-
-    // получение кода состояния записи файла сервером
-        uint8_t state_file_write;
-        res = recv(s, &state_file_write, sizeof(state_file_write), 0);
-        if (res < 0 || res != sizeof(state_file_write))
-        {
-            std::cerr << "Recv call error state_file_write. " << strerror(errno) << "\n";
-            return 1;
-        }
-
-        if (state_file_write == 128)
-        {
-            std::string error_message;
-            if (receiveString(s, error_message))
-            {
-                std::cerr << "Receive string error_message error.\n";
-                return 1;
-            }
-            std::cerr << error_message << "\n";
-            return 1;
-        }
-        else if (state_file_write == 129)
-        {
-            return 0;
-        }
-        else
-        {
-            std::cerr << "Unknown command: " << state_file_write << "\n";
-            return 1;
-        }
-    }
-    else
+    else if (file_name_allow != 129)
     {
         std::cerr << "Unknown command: " << file_name_allow << "\n";
         return 1;
     }
+
+// определение длины файла
+    struct stat st_buff;
+    res = stat(file_name.c_str(), &st_buff);
+    if (res < 0)
+    {
+        std::cerr << "Stat call error. " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    uint32_t filesize = st_buff.st_size;
+
+//отправка серверу длины файла
+    res = send(s, &filesize, sizeof(filesize), 0);
+    if (res < 0 || res != sizeof(filesize))
+    {
+        std::cerr << "Send call error file size. " << strerror(errno) << "\n";
+        return 1;
+    }
+
+//открытие клиентом файла
+    std::ifstream fin(file_name);
+    if (!fin)
+    {
+        std::cerr << file_name << "File not open.\n";
+        return 1;
+    }
+
+// чтение файла в буфер
+    char buff[1024] = {0};
+    while (!fin.eof())
+    {
+        fin.read(buff, 1024);
+
+// отправка содержимого буфера серверу
+        if (fin.gcount() > 0)
+        {
+            res = send(s, buff, fin.gcount(), 0);
+            if (res < 0 || res != (int)fin.gcount())
+            {
+                std::cerr << "Send call error buff. " << strerror(errno) << "\n";
+                return 1;
+            }
+        }
+
+    }
+
+// получение кода состояния записи файла сервером
+    uint8_t state_file_write;
+    res = recv(s, &state_file_write, sizeof(state_file_write), 0);
+    if (res < 0 || res != sizeof(state_file_write))
+    {
+        std::cerr << "Recv call error state_file_write. " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    if (state_file_write == 128)
+    {
+        std::string error_message;
+        if (receiveString(s, error_message))
+        {
+            std::cerr << "Receive string error_message error.\n";
+            return 1;
+        }
+        std::cerr << error_message << "\n";
+        return 1;
+    }
+    else if (state_file_write != 129)
+    {
+        std::cerr << "Unknown command: " << state_file_write << "\n";
+        return 1;
+    }
+    return 0;
 }
 
 int main(int argc, char* argv[])
