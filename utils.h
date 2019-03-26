575b54efd496051a1410ef4d75c478604e2b46a6
Elena Mamontova <helenh463@gmail.com>
2019-03-26 11:15:02 +0200

    Parameter 'command' replaced to 'source' in the sendUint8() function. Parameter 'filesize' replaced to 'source' in the sendUint32() function. Parameter 'command' replaced to 'destination' in the receiveUint8() function. Parameter 'filesize' replaced to 'destination' in the receiveUint32() function.

--- utils.h
+++ utils.h
@@ -8,10 +8,10 @@
 bool setAddress(const std::string& address, struct sockaddr_in* local);
 int sendString(int sock, const std::string& source);
 int receiveString(int sock, std::string& destination);
-int sendUint8(int sock, uint8_t command);
-int receiveUint8(int sock, uint8_t& command);
-int sendUint32(int sock, uint32_t filesize);
-int receiveUint32(int sock, uint32_t& filesize);
+int sendUint8(int sock, uint8_t source);
+int receiveUint8(int sock, uint8_t& destination);
+int sendUint32(int sock, uint32_t source);
+int receiveUint32(int sock, uint32_t& destination);
 
 enum command {GET, PUT, LIST, ERROR = 128, SUCCESS, SEND_FILE, SEND_LIST};
 
