1a7ff47e3885d9cb066b94be5a1d5971427b1f5d
Elena Mamontova <helenh463@gmail.com>
2019-05-29 14:00:20 +0300

    Parameter int sock replaced by Socket& sock in sendString, receiveString, sendUint8, receiveUint8,  sendUint32, receiveUint32 functions. Directive

--- utils.h
+++ utils.h
@@ -5,13 +5,15 @@
 #include <netinet/in.h> //struct sockaddr_in
 #include <cstdint> //uint8_t, uint32_t
 
+#include "socket.h"
+
 bool setAddress(const std::string& address, struct sockaddr_in* local);
-int sendString(int sock, const std::string& source);
-int receiveString(int sock, std::string& destination);
-int sendUint8(int sock, uint8_t source);
-int receiveUint8(int sock, uint8_t& destination);
-int sendUint32(int sock, uint32_t source);
-int receiveUint32(int sock, uint32_t& destination);
+int sendString(Socket& sock, const std::string& source);
+int receiveString(Socket& sock, std::string& destination);
+int sendUint8(Socket& sock, uint8_t source);
+int receiveUint8(Socket& sock, uint8_t& destination);
+int sendUint32(Socket& sock, uint32_t source);
+int receiveUint32(Socket& sock, uint32_t& destination);
 
 enum Command {GET = 0, PUT = 1, LIST = 2, ERROR = 128, SUCCESS = 129, SEND_FILE = 130, SEND_LIST = 131};
 
