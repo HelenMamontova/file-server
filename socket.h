76bab4dd55abf180d91e48d3cc4c4141cfb88559
Elena Mamontova <helenh463@gmail.com>
2019-06-11 17:59:10 +0300

    Socket(int fd) constructor moved into the private section. recvSocket, sendSocket, bindSocket, listenSocket, connectSocket, acceptSocket methods changed to recv, send, bind, listen, connect, accept methods.

--- socket.h
+++ socket.h
@@ -8,21 +8,21 @@ private:
     Socket(const Socket&);
     Socket& operator = (const Socket&);
 
+    Socket(int fd);
+
 public:
     Socket();
     ~Socket();
 
-    Socket(int fd);
-
     Socket(Socket&& other);
     Socket& operator = (Socket&& other);
 
-    int bindSocket(const sockaddr_in &addr, size_t addrlen);
-    int listenSocket(int n);
-    int sendSocket(const void *buf, size_t len, int n);
-    int recvSocket(void *buf, size_t len, int n);
-    int connectSocket(const sockaddr_in &addr, size_t addrlen);
-    Socket acceptSocket(const sockaddr_in &addr, int addrlen);
+    int bind(const sockaddr_in &addr, size_t addrlen);
+    int listen(int n);
+    int send(const void *buf, size_t len, int n);
+    int recv(void *buf, size_t len, int n);
+    int connect(const sockaddr_in &addr, size_t addrlen);
+    Socket accept(const sockaddr_in &addr, int addrlen);
 };
 
 #endif
