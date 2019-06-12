d011aa848b8cca0ee2ed1c3b95b32ee806ceb5e2
Elena Mamontova <helenh463@gmail.com>
2019-06-12 14:51:50 +0300

    Class member variable s changed to m_var.

--- socket.cpp
+++ socket.cpp
@@ -11,25 +11,25 @@
 
 Socket::Socket()
 {
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    m_var = socket(AF_INET, SOCK_STREAM, 0);
 }
 
 Socket::Socket(int fd)
 {
-    s = fd;
+    m_var = fd;
 }
 
 Socket::~Socket()
 {
-    close(s);
+    close(m_var);
 }
 
 // move constructor
 Socket::Socket(Socket&& other)
-    : s(0)
+    : m_var(0)
 {
-    s = other.s;
-    other.s = 0;
+    m_var = other.m_var;
+    other.m_var = 0;
 }
 
 // move asignment operator
@@ -37,8 +37,8 @@ Socket& Socket::operator = (Socket&& other)
 {
     if (this != &other)
     {
-        s = other.s;
-        other.s = 0;
+        m_var = other.m_var;
+        other.m_var = 0;
         return *this;
     }
     return *this;
@@ -46,30 +46,30 @@ Socket& Socket::operator = (Socket&& other)
 
 int Socket::bind(const sockaddr_in &addr, size_t addrlen)
 {
-    return ::bind(s, (const sockaddr*) &addr, addrlen);
+    return ::bind(m_var, (const sockaddr*) &addr, addrlen);
 }
 
 int Socket::listen(int n)
 {
-    return ::listen(s, n);
+    return ::listen(m_var, n);
 }
 
 int Socket::send(const void *buf, size_t len, int n)
 {
-    return ::send(s, buf, len, n);
+    return ::send(m_var, buf, len, n);
 }
 
 int Socket::recv(void *buf, size_t len, int n)
 {
-    return ::recv(s, buf, len, n);
+    return ::recv(m_var, buf, len, n);
 }
 
 int Socket::connect(const sockaddr_in &addr, size_t addrlen)
 {
-    return ::connect(s, (const sockaddr*) &addr, addrlen);
+    return ::connect(m_var, (const sockaddr*) &addr, addrlen);
 }
 
 Socket Socket::accept(const sockaddr_in &addr, int addrlen)
 {
-    return Socket(::accept(s, (sockaddr*) &addr, (socklen_t*) &addrlen));
+    return Socket(::accept(m_var, (sockaddr*) &addr, (socklen_t*) &addrlen));
 }
