f9a82e016d4dfd022f411530b70bf0378303bf9d
Elena Mamontova <helenh463@gmail.com>
2019-07-19 15:16:57 +0300

    The include <stdexcept> directive added. Class Error added into the public section.

--- socket.h
+++ socket.h
@@ -2,6 +2,7 @@
 #define SOCKET_H
 
 #include <string>
+#include <stdexcept>
 #include <netinet/in.h> //struct sockaddr_in
 #include <linux/in.h> //struct sockaddr_in
 
@@ -30,6 +31,15 @@ public:
     int recv(void *buf, size_t len, int n);
     int connect(const sockaddr_in &addr, size_t addrlen);
     Socket accept(sockaddr_in &addr, socklen_t &addrlen);
+
+    class Error: public std::runtime_error
+    {
+    public:
+       explicit Error(std::string error_message) noexcept
+        : runtime_error(error_message)
+        {
+        }
+    };
 };
 
 #endif
