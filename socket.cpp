7e93a5ccfe98864e1797310d1ce1d82268e6f267
Elena Mamontova <helenh463@gmail.com>
2019-08-13 16:21:17 +0300

    The #include <cstring>, #include <cerrno> header files added. The errno macro added in the error message when throwing an exception in the Socket() constructor, bind(), listen(), send(), recv(), connect(), accept() methods.

--- socket.cpp
+++ socket.cpp
@@ -3,13 +3,15 @@
 
 #include <sys/types.h> //socket, bind
 #include <sys/socket.h> //socket, bind, accept, listen
+#include <cstring> //strerror
+#include <cerrno>
 #include <unistd.h> //close
 
 Socket::Socket()
     : m_sock(socket(AF_INET, SOCK_STREAM, 0))
 {
     if (m_sock == -1)
-        throw Error("Error creating socket.");
+        throw Error("Error creating socket. " + std::string(strerror(errno)));
 }
 
 Socket::Socket(int fd)
@@ -65,20 +67,20 @@ void Socket::connect(const std::string &address)
 void Socket::bind(const sockaddr_in &addr, size_t addrlen)
 {
     if (::bind(m_sock, (const sockaddr*) &addr, addrlen))
-        throw Error("Error bind.");
+        throw Error("Error bind. " + std::string(strerror(errno)));
 }
 
 void Socket::listen(int n)
 {
     if (::listen(m_sock, n))
-        throw Error("Error listen.");
+        throw Error("Error listen. " + std::string(strerror(errno)));
 }
 
 size_t Socket::send(const void *buf, size_t len, int n)
 {
     int res = ::send(m_sock, buf, len, n);
     if (res < 0)
-        throw Error("Error send.");
+        throw Error("Error send. " + std::string(strerror(errno)));
     return res;
 }
 
@@ -86,20 +88,20 @@ size_t Socket::recv(void *buf, size_t len, int n)
 {
     int res = ::recv(m_sock, buf, len, n);
     if (res < 0)
-        throw Error("Error recv.");
+        throw Error("Error recv. " + std::string(strerror(errno)));
     return res;
 }
 
 void Socket::connect(const sockaddr_in &addr, size_t addrlen)
 {
     if (::connect(m_sock, (const sockaddr*) &addr, addrlen))
-        throw Error("Error connect.");
+        throw Error("Error connect. " + std::string(strerror(errno)));
 }
 
 Socket Socket::accept(sockaddr_in &addr, socklen_t &addrlen)
 {
     int fd = ::accept(m_sock, (sockaddr*) &addr, &addrlen);
     if (fd == -1)
-        throw Error("Error accept.");
+        throw Error("Error accept. " + std::string(strerror(errno)));
     return Socket(fd);
 }
