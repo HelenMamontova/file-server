122706325802c37da3e4921681d01edad3fe893c
Elena Mamontova <helenh463@gmail.com>
2019-08-15 13:12:56 +0300

    The return value type of setAddress() function changed to void.

--- utils.h
+++ utils.h
@@ -8,7 +8,7 @@
 #include <netinet/in.h> //struct sockaddr_in
 #include <linux/in.h> //struct sockaddr_in
 
-bool setAddress(const std::string& address, struct sockaddr_in* local);
+void setAddress(const std::string& address, struct sockaddr_in* local);
 void sendString(Socket& sock, const std::string& source);
 std::string receiveString(Socket& sock);
 void sendUint8(Socket& sock, uint8_t source);
