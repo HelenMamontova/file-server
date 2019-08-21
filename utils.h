d5d0e9028f31830b8e879362bd1abf756a963463
Elena Mamontova <helenh463@gmail.com>
2019-08-21 13:49:53 +0300

    The setAddress function renamed to makeAddress.

--- utils.h
+++ utils.h
@@ -8,7 +8,7 @@
 #include <netinet/in.h> //struct sockaddr_in
 #include <linux/in.h> //struct sockaddr_in
 
-void setAddress(const std::string& address, struct sockaddr_in* local);
+void makeAddress(const std::string& address, struct sockaddr_in* local);
 
 enum Command {GET = 0, PUT = 1, LIST = 2, ERROR = 128, SUCCESS = 129, SEND_FILE = 130, SEND_LIST = 131};
 
