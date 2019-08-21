3d9c649a3318003e0fd7459ab387a586824d9f10
Elena Mamontova <helenh463@gmail.com>
2019-08-21 13:51:10 +0300

    The setAddress function renamed to makeAddress.

--- utils.cpp
+++ utils.cpp
@@ -3,11 +3,11 @@
 #include <cstring>
 #include <arpa/inet.h> //inet_aton
 
-void setAddress(const std::string& address, struct sockaddr_in* local)
+void makeAddress(const std::string& address, struct sockaddr_in* local)
 {
     size_t pos = address.find(":");
     if (pos == std::string::npos)
-        throw Socket::Error("Error setAddress: Incorrect address: " + address);
+        throw Socket::Error("Error makeAddress: Incorrect address: " + address);
 
     std::string ip_address = address.substr(0, pos);
     std::string port = address.substr(pos + 1, address.length() - pos - 1);
@@ -16,12 +16,12 @@ void setAddress(const std::string& address, struct sockaddr_in* local)
     local->sin_family = AF_INET;
 
     if (!inet_aton(ip_address.c_str(), &local->sin_addr))
-        throw Socket::Error("Error setAddress: Unknown host: " + ip_address);
+        throw Socket::Error("Error makeAddress: Unknown host: " + ip_address);
 
     char* endptr;
     short port_num = strtol(port.c_str(), &endptr, 10);
     if (*endptr != '\0')
-        throw Socket::Error("Error setAddress: Unknown port: " + port);
+        throw Socket::Error("Error makeAddress: Unknown port: " + port);
 
     local->sin_port = htons(port_num);
 }
