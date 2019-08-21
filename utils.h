15a01dea95db16003d3c0d8e4cbce2e3661fad09
Elena Mamontova <helenh463@gmail.com>
2019-08-21 12:55:33 +0300

    The sendString, receiveString, sendUin8, receiveUnt8, sendUint32, receiveYint32 functions declaration removed.

--- utils.h
+++ utils.h
@@ -9,12 +9,6 @@
 #include <linux/in.h> //struct sockaddr_in
 
 void setAddress(const std::string& address, struct sockaddr_in* local);
-void sendString(Socket& sock, const std::string& source);
-std::string receiveString(Socket& sock);
-void sendUint8(Socket& sock, uint8_t source);
-uint8_t receiveUint8(Socket& sock);
-void sendUint32(Socket& sock, uint32_t source);
-uint32_t receiveUint32(Socket& sock);
 
 enum Command {GET = 0, PUT = 1, LIST = 2, ERROR = 128, SUCCESS = 129, SEND_FILE = 130, SEND_LIST = 131};
 
