4a96726373db60d9168b6a4217d42f155e4c65f6
Elena Mamontova <helenh463@gmail.com>
2019-08-23 14:20:16 +0300

    The sockaddr_in makeAddress function declaration removed. All #include directives removed.

--- utils.h
+++ utils.h
@@ -1,14 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include "socket.h"
-
-#include <string>
-#include <cstdint> //uint8_t, uint32_t
-#include <netinet/in.h> //struct sockaddr_in
-
-sockaddr_in makeAddress(const std::string& address);
-
 enum Command {GET = 0, PUT = 1, LIST = 2, ERROR = 128, SUCCESS = 129, SEND_FILE = 130, SEND_LIST = 131};
 
 #endif
